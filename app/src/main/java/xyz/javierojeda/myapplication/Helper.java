e10b112c6f372f31a995e57424b86be9d549629a
Javier Ojeda <ojeda.javier94@gmail.com>
2017-12-16 18:56:00 -0600

    initial commit

---
+++ app/src/main/java/xyz/javierojeda/myapplication/Helper.java
@@ -0,0 +1,12 @@
+package xyz.javierojeda.myapplication;
+
+/**
+ * Created by javierojeda on 16/12/17.
+ */
+
+public class Helper {
+
+    public static int doubleTheNumber(int number) {
+        return number * 2;
+    }
+}
